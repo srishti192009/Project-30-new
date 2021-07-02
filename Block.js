1b1e6383b349e53f0b5c22988fdc7658bcdff989
srishti192009 <80687916+srishti192009@users.noreply.github.com>
2021-07-02 07:59:38 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block{
+    constructor(x, y, width, height) {
+      
+        var options = {
+           
+            restitution :0.4,
+            friction :1.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visiblity = 255;
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed < 3){
+          
+      super.display();
+  
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+           pop();
+         }
+         
+       
+        
+      }
+}
